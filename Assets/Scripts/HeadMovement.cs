129c74197ce5f5870620b0301746f07e63de63ca
zexui <mmmfred34@gmail.com>
2018-11-01 15:49:49 +0100

    SnakeGrow

---
+++ Assets/Scripts/HeadMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadMovement : MonoBehaviour {
+
+    Vector2 retning;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Movement", 0.1f, 0.1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            retning =  Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            retning = -Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            retning = -Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            retning = Vector2.right;
+        }
+    }
+
+    void Movement ()
+    {
+        transform.Translate(retning);
+    }
+}
---
+++ Assets/Scripts/MadLaver.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MadLaver : MonoBehaviour {
+
+    public GameObject Mad;
+    GameObject MadSpawn;
+
+    public Transform Top;
+    public Transform Bund;
+    public Transform Højre;
+    public Transform Venstre;
+
+	// Use this for initialization
+	void Start () {
+
+        int x = (int)Random.Range(Venstre.position.x, Højre.position.x);
+
+        int y = (int)Random.Range(Bund.position.y, Top.position.y);
+
+        MadSpawn = Instantiate(Mad, new Vector2(x, y), Quaternion.identity);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Dette virker");
+
+        Destroy(MadSpawn);
+
+        int x = (int)Random.Range(Venstre.position.x, Højre.position.x);
+
+        int y = (int)Random.Range(Bund.position.y, Top.position.y);
+
+        MadSpawn = Instantiate(Mad, new Vector2(x, y), Quaternion.identity);
+
+    }
+
+}

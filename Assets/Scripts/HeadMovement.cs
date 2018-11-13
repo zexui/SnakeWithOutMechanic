23020a2d5aa0597d0c19b7488ddf9be44b04d035
zexui <mmmfred34@gmail.com>
2018-11-13 15:15:04 +0100

    Made snake grow when eating
    The snake grows when it eats a fruit

--- Assets/Scripts/HeadMovement.cs
+++ Assets/Scripts/HeadMovement.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class HeadMovement : MonoBehaviour {
 
     Vector2 retning;
 
-	// Use this for initialization
-	void Start () {
+    List<Transform> Hale = new List<Transform>();
+
+    bool Spist = false;
+    public GameObject HalePrefab;
+
+    public GameObject Snake;
+    Vector2 Startpos = new Vector2(0.0f, 0.0f); 
+
+    // Use this for initialization
+    void Start () {
         InvokeRepeating("Movement", 0.1f, 0.1f);
 	}
 	
@@ -26,7 +35,7 @@ public class HeadMovement : MonoBehaviour {
         {
             retning = -Vector2.right;
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             retning = Vector2.right;
         }
@@ -34,6 +43,43 @@ public class HeadMovement : MonoBehaviour {
 
     void Movement ()
     {
+        Vector2 CurrentPos = transform.position;
+
         transform.Translate(retning);
+
+        if (Spist == true)
+        {
+            GameObject HalePre = (GameObject)Instantiate(HalePrefab, CurrentPos, Quaternion.identity);
+
+            Hale.Insert(0, HalePre.transform);
+
+            Spist = false;
+
+        }
+        else if (Hale.Count > 0)
+        {
+            Hale.Last().position = CurrentPos;
+
+            Hale.Insert(0, Hale.Last());
+            Hale.RemoveAt(Hale.Count - 1);
+        }
+
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.name.StartsWith("HvidCirkel"))
+        {
+            Spist = true;
+
+        }
+        else
+        {
+            transform.position = Startpos;
+        }
+
+    }
+
 }
+

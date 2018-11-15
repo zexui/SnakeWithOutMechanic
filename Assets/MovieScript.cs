5879fc31e1197e0bb3ca20540a8eee22062607ff
zexui <mmmfred34@gmail.com>
2018-11-15 12:45:34 +0100

    Added all scenes
    Made the scenes for the game so it has a menu

---
+++ Assets/MovieScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MovieScript : MonoBehaviour {
+
+    float MovieTime = 0;
+
+    // Use this for initialization
+    void Start () {
+
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        
+        MovieTime += Time.deltaTime;
+
+        if (MovieTime >= 8)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    public void Play()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
